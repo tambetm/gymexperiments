715494dd26bd4a908571ee98dbdd1f27908c8425
Tambet Matiisen <tambet.matiisen@gmail.com>
2016-05-05 23:46:43 +0300

    test.py samples randomly and accepts environment name as argument.

--- test.py
+++ test.py
@@ -1,15 +1,19 @@
+import argparse
 import gym
 import numpy as np
 import time
 
-env = gym.make('Pendulum-v0')
+parser = argparse.ArgumentParser()
+parser.add_argument('environment')
+args = parser.parse_args()
+
+env = gym.make(args.environment)
 for i_episode in xrange(20):
     observation = env.reset()
     action = np.zeros((1,))
     for t in xrange(100):
         env.render()
-        print observation, -env.max_torque, env.max_torque
-        action[0] = -1/observation[1]
+        action = env.action_space.sample()
         observation, reward, done, info = env.step(action)
         if done:
             print "Episode finished after {} timesteps".format(t+1)
