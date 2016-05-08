591b7bf31740e25444bb55e919a1cbe92f25f012
Tambet Matiisen <tambet.matiisen@gmail.com>
2016-05-08 08:45:13 +0300

    Added regularization and norm constraints. Also added some comments to code.

--- naf.py
+++ naf.py
@@ -4,6 +4,8 @@ from gym.spaces import Box, Discrete
 from keras.models import Model
 from keras.layers import Input, Dense, Lambda, Reshape, merge
 from keras.layers.normalization import BatchNormalization
+from keras.regularizers import l2, activity_l2
+from keras.constraints import maxnorm, unitnorm
 from keras.optimizers import Adam, RMSprop
 from keras import backend as K
 import theano.tensor as T
@@ -15,6 +17,10 @@ parser.add_argument('--hidden_size', type=int, default=100)
 parser.add_argument('--layers', type=int, default=2)
 parser.add_argument('--batch_norm', action="store_true", default=False)
 parser.add_argument('--no_batch_norm', action="store_false", dest="batch_norm")
+parser.add_argument('--max_norm', type=int)
+parser.add_argument('--unit_norm', action='store_true', default=False)
+parser.add_argument('--l2_reg', type=float)
+parser.add_argument('--l1_reg', type=float)
 parser.add_argument('--min_train', type=int, default=10)
 parser.add_argument('--train_repeat', type=int, default=10)
 parser.add_argument('--gamma', type=float, default=0.99)
@@ -24,25 +30,47 @@ parser.add_argument('--max_timesteps', type=int, default=200)
 parser.add_argument('--activation', choices=['tanh', 'relu'], default='tanh')
 parser.add_argument('--optimizer', choices=['adam', 'rmsprop'], default='adam')
 parser.add_argument('--optimizer_lr', type=float, default=0.001)
-parser.add_argument('--noise', choices=['linear_decay', 'exp_decay', 'fixed', 'covariance'], default='covariance')
-parser.add_argument('--noise_scale', type=float, default=0.001)
+parser.add_argument('--noise', choices=['linear_decay', 'exp_decay', 'fixed', 'covariance'], default='linear_decay')
+parser.add_argument('--noise_scale', type=float, default=0.01)
 parser.add_argument('--display', action='store_true', default=True)
 parser.add_argument('--no_display', dest='display', action='store_false')
 parser.add_argument('--gym_record')
 parser.add_argument('environment')
 args = parser.parse_args()
 
+assert K._BACKEND == 'theano', "only works with Theano as backend"
+
+# create environment
 env = gym.make(args.environment)
-assert isinstance(env.observation_space, Box)
-assert isinstance(env.action_space, Box)
+assert isinstance(env.observation_space, Box), "observation space must be continuous"
+assert isinstance(env.action_space, Box), "action space must be continuous"
 assert len(env.action_space.shape) == 1
 num_actuators = env.action_space.shape[0]
 print "num_actuators:", num_actuators
 
+# start monitor for OpenAI Gym
 if args.gym_record:
   env.monitor.start(args.gym_record)
 
+# optional norm constraint
+if args.max_norm:
+  W_constraint = maxnorm(args.max_norm)
+elif args.unit_norm:
+  W_constraint = unitnorm()
+else:
+  W_constraint = None
+
+# optional regularizer
+if args.l2_reg:
+  W_regularizer = l2(args.l2_reg)
+elif args.l1_reg:
+  W_regularizer = l1(args.l1_reg)
+else:
+  W_regularizer = None
+
+# helper functions to use with layers
 if num_actuators == 1:
+  # simpler versions for single actuator case
   def _L(x):
     return K.exp(x)
 
@@ -57,6 +85,7 @@ if num_actuators == 1:
     v, a = t
     return v + a
 else:
+  # use Theano advanced operators for multiple actuator case
   def _L(x):
     # initialize with zeros
     batch_size = x.shape[0]
@@ -86,6 +115,7 @@ else:
     v, a = t
     return v + a
 
+# helper function to produce layers twice
 def createLayers():
   x = Input(shape=env.observation_space.shape, name='x')
   u = Input(shape=env.action_space.shape, name='u')
@@ -94,12 +124,14 @@ def createLayers():
   else:
     h = x
   for i in xrange(args.layers):
-    h = Dense(args.hidden_size, activation=args.activation, name='h'+str(i+1))(h)
+    h = Dense(args.hidden_size, activation=args.activation, name='h'+str(i+1),
+        W_constraint=W_constraint, W_regularizer=W_regularizer)(h)
     if args.batch_norm and i != args.layers - 1:
       h = BatchNormalization()(h)
-  v = Dense(1, name='v')(h)
-  m = Dense(num_actuators, name='m')(h)
-  l0 = Dense(num_actuators * (num_actuators + 1)/2, name='l0')(h)
+  v = Dense(1, name='v', W_constraint=W_constraint, W_regularizer=W_regularizer)(h)
+  m = Dense(num_actuators, name='m', W_constraint=W_constraint, W_regularizer=W_regularizer)(h)
+  l0 = Dense(num_actuators * (num_actuators + 1)/2, name='l0',
+        W_constraint=W_constraint, W_regularizer=W_regularizer)(h)
   l = Lambda(_L, output_shape=(num_actuators, num_actuators), name='l')(l0)
   p = Lambda(_P, output_shape=(num_actuators, num_actuators), name='p')(l)
   a = merge([m, p, u], mode=_A, output_shape=(None, num_actuators,), name="a")
@@ -108,6 +140,7 @@ def createLayers():
 
 x, u, m, v, q, p, a = createLayers()
 
+# wrappers around computational graph
 fmu = K.function([K.learning_phase(), x], m)
 mu = lambda x: fmu([0, x])
 
@@ -120,6 +153,7 @@ A = lambda x, u: fA([0, x, u])
 fQ = K.function([K.learning_phase(), x, u], q)
 Q = lambda x, u: fQ([0, x, u])
 
+# main model
 model = Model(input=[x,u], output=q)
 model.summary()
 
@@ -131,20 +165,25 @@ else:
   assert False
 model.compile(optimizer=optimizer, loss='mse')
 
+# another set of layers for target model
 x, u, m, v, q, p, a = createLayers()
 
+# V() function uses target model weights
 fV = K.function([K.learning_phase(), x], v)
 V = lambda x: fV([0, x])
 
+# target model is initialized from main model
 target_model = Model(input=[x,u], output=q)
 target_model.set_weights(model.get_weights())
 
+# replay memory
 prestates = []
 actions = []
 rewards = []
 poststates = []
 terminals = []
 
+# the main learning loop
 total_reward = 0
 for i_episode in xrange(args.episodes):
     observation = env.reset()
@@ -154,8 +193,11 @@ for i_episode in xrange(args.episodes):
         if args.display:
           env.render()
 
+        # predict the mean action from current observation
         x = np.array([observation])
         u = mu(x)[0]
+
+        # add exploration noise to the action
         if args.noise == 'linear_decay':
           action = u + np.random.randn(num_actuators) / (i_episode + 1)
         elif args.noise == 'exp_decay':
@@ -175,33 +217,43 @@ for i_episode in xrange(args.episodes):
           assert False
         #print "action:", action, "Q:", Q(x, np.array([action])), "V:", V(x)
         #print "action:", action, "advantage:", A(x, np.array([action]))
+        #print "mu:", u, "action:", action
+        #print "Q(mu):", Q(x, np.array([u])), "Q(action):", Q(x, np.array([action]))
 
+        # add current observation and action to replay memory
         prestates.append(observation)
         actions.append(action)
         #print "prestate:", observation
 
+        # take the action and record reward
         observation, reward, done, info = env.step(action)
         episode_reward += reward
         #print "reward:", reward
         #print "poststate:", observation
 
+        # add reward, resulting observation and terminal indicator to replay memory
         rewards.append(reward)
         poststates.append(observation)
         terminals.append(done)
         #print "done:", done
 
+        # start training after min_train experiences are recorded in replay memory
         if len(prestates) > args.min_train:
           loss = 0
+          # perform train_repeat Q-updates
           for k in xrange(args.train_repeat):
+            # if less experiences than batch size, then use all of them
             if len(prestates) > args.batch_size:
               indexes = np.random.choice(len(prestates), size=args.batch_size)
             else:
               indexes = range(len(prestates))
 
+            # Q-update
             v = V(np.array(poststates)[indexes])
             y = np.array(rewards)[indexes] + args.gamma * np.squeeze(v)
             loss += model.train_on_batch([np.array(prestates)[indexes], np.array(actions)[indexes]], y)
 
+            # copy weights to target model, averaged by tau
             weights = model.get_weights()
             target_weights = target_model.get_weights()
             for i in xrange(len(weights)):
--- naf_search.sh
+++ naf_search.sh
@@ -5,8 +5,9 @@ python naf.py --gym_record ${1}_reference $*
 python naf.py --optimizer_lr 0.01 --gym_record ${1}_lr0.01 $*
 python naf.py --optimizer_lr 0.0001 --gym_record ${1}_lr0.0001 $*
 python naf.py --noise exp_decay --gym_record ${1}_noise_exp $*
-python naf.py --noise fixed --fixed_noise 0.1 --gym_record ${1}_noise_fixed0.1 $*
-python naf.py --noise fixed --fixed_noise 0.01 --gym_record ${1}_noise_fixed0.01 $*
+python naf.py --noise fixed --noise_scale 0.1 --gym_record ${1}_noise_fixed0.1 $*
+python naf.py --noise fixed --noise_scale 0.01 --gym_record ${1}_noise_fixed0.01 $*
+python naf.py --noise covariance --noise_scale 0.001 --gym_record ${1}_noise_cov $*
 #python naf.py --batch_size 1000 --gym_record ${1}_batchsize1000 $*
 python naf.py --tau 1 --gym_record ${1}_tau1 $*
 python naf.py --tau 0.1 --gym_record ${1}_tau0.1 $*
@@ -14,4 +15,7 @@ python naf.py --tau 0.01 --gym_record ${1}_tau0.01 $*
 python naf.py --activation relu --gym_record ${1}_relu $*
 python naf.py --layers 1 --gym_record ${1}_layers1 $*
 python naf.py --hidden_size 50 --gym_record ${1}_hidden50 $*
-python naf.py --gamma 0.99 --gym_record ${1}_gamma0.99 $*
+python naf.py --gamma 0.995 --gym_record ${1}_gamma0.995 $*
+python naf.py --unit_norm --gym_record ${1}_unitnorm $*
+python naf.py --max_norm 1 --gym_record ${1}_maxnorm1 $*
+python naf.py --max_norm 5 --gym_record ${1}_maxnorm5 $*
