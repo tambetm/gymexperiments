e001fdb5b6b74d03e473e683eff0309385caea82
Tambet Matiisen <tambet.matiisen@gmail.com>
2016-10-30 08:17:19 -0700

    Windows compatibility + some refactorizations.

--- a3c_process.py
+++ a3c_process.py
@@ -8,14 +8,14 @@ from queue import Empty
 import cPickle as pickle
 
 from keras.models import Model
-from keras.layers import Input, Dense, Masking, TimeDistributed
+from keras.layers import Input, Masking, TimeDistributed, Dense
 from keras.objectives import categorical_crossentropy
 from keras.utils import np_utils
 import keras.backend as K
 import numpy as np
 
 
-def create_model(env):
+def create_model(env, args):
     x = Input(shape=(None,) + env.observation_space.shape, name="x")
     # apply masking so that shorter episodes in batch can be padded
     # padded inputs must contain all zeros
@@ -24,22 +24,22 @@ def create_model(env):
     # policy network
     for i in xrange(args.layers):
         h = TimeDistributed(Dense(args.hidden_size, activation=args.activation), name="h%d" % (i + 1))(h)
-    y = TimeDistributed(Dense(env.action_space.n, activation='softmax'), name="y")(h)
+    p = TimeDistributed(Dense(env.action_space.n, activation='softmax'), name="p")(h)
 
     # baseline network
     h = TimeDistributed(Dense(args.hidden_size, activation=args.activation), name="hb")(h)
     b = TimeDistributed(Dense(1), name="b")(h)
 
     # total reward is additional input
-    R = Input(shape=(None, 1))
+    g = Input(shape=(None, 1))
 
     # policy gradient loss
     def policy_gradient_loss(l_sampled, l_predicted):
-        return K.mean(K.stop_gradient(R - b) * categorical_crossentropy(l_sampled, l_predicted)[..., np.newaxis], axis=-1)
+        return K.mean(K.stop_gradient(g - b) * categorical_crossentropy(l_sampled, l_predicted)[..., np.newaxis], axis=-1)
 
     # inputs to the model are observation and total reward,
     # outputs are action probabilities and baseline
-    model = Model(input=[x, R], output=[y, b])
+    model = Model(input=[x, g], output=[p, b])
 
     # baseline is optimized with MSE
     model.compile(optimizer=args.optimizer, loss=[policy_gradient_loss, 'mse'], loss_weights=[1, args.tau])
@@ -48,18 +48,38 @@ def create_model(env):
     return model
 
 
-def runner(shared_array, fifo):
+def predict(model, observation):
+    # create inputs for batch (and timestep) of size 1
+    x = np.array([[observation]])
+    g = np.zeros((1, 1, 1))  # dummy return
+    # predict action probabilities (and baseline state value)
+    y, b = model.predict([x, g], batch_size=1)
+    # return action probabilities and baseline
+    return y[0, 0], b[0, 0, 0]
+
+
+def discount(rewards, g, gamma):
+    # calculate discounted future rewards for this episode
+    returns = []
+    for r in reversed(rewards):
+        g = r + g * gamma
+        returns.insert(0, g)
+    #print returns
+    return returns
+
+
+def runner(shared_buffer, fifo, args):
     # local environment for runner
     env = gym.make(args.environment)
     # copy of model
-    model = create_model(env)
+    model = create_model(env, args)
 
     done = True
     for episode in range(args.max_episodes):
         # copy weights from main network at the beginning of episode
         # the main network's weights are only read, never modified
         # but we create our own model instance, because Keras is not thread-safe
-        model.set_weights(pickle.loads(shared_array.raw))
+        model.set_weights(pickle.loads(shared_buffer.raw))
 
         observations = []
         actions = []
@@ -72,17 +92,14 @@ def runner(shared_array, fifo):
             if args.display:
                 env.render()
 
-            # create inputs for batch (and timestep) of size 1
-            x = np.array([[observation]])
-            R = np.zeros((1, 1, 1))  # dummy return
             # predict action probabilities (and baseline state value)
-            y, b = model.predict([x, R], batch_size=1)
+            p, b = predict(model, observation)
             #print "b:", b[0][0]
-            #print "y:", y[0][0]
+            #print "p:", p[0][0]
 
             # sample action using those probabilities
-            y /= np.sum(y)  # ensure y-s sum up to 1
-            action = np.random.choice(env.action_space.n, p=y[0][0])
+            p /= np.sum(p)  # ensure p-s sum up to 1
+            action = np.random.choice(env.action_space.n, p=p)
             #print "action:", action
 
             # step environment and log data
@@ -95,22 +112,22 @@ def runner(shared_array, fifo):
             if done:
                 break
 
-        # send observations, actions and rewards
+        # calculate discounted returns
+        if done:
+            # if terminal state then start from 0
+            returns = discount(rewards, 0, args.gamma)
+        else:
+            # otherwise calculate the value of the last state
+            _, v = predict(model, observation)
+            #print "v:", v
+            returns = discount(rewards, v, args.gamma)
+
+        # send observations, actions, rewards and returns
         # block if fifo is full
-        fifo.put((observations, actions, rewards, done, observation))
+        fifo.put((observations, actions, rewards, returns))
 
 
-def discount(rewards, g=0):
-    # calculate discounted future rewards for this episode
-    returns = []
-    for r in reversed(rewards):
-        g = r + g * args.gamma
-        returns.insert(0, g)
-    #print returns
-    return returns
-
-
-def trainer(model, fifos, shared_array):
+def trainer(model, fifos, shared_buffer, args):
     step = 0
     while len(multiprocessing.active_children()) > 0:
         batch_observations = []
@@ -124,19 +141,7 @@ def trainer(model, fifos, shared_array):
         for fifo in fifos:
             try:
                 # wait for new episode
-                observations, actions, rewards, done, last_obs = fifo.get(timeout=args.queue_timeout)
-                # calculate discounted returns
-                if done:
-                    # if terminal state then start from 0
-                    returns = discount(rewards, 0)
-                else:
-                    # otherwise calculate the value of the last state
-                    x = np.array([[last_obs]])
-                    R = np.zeros((1, 1, 1))  # dummy return
-                    # predict action probabilities (and baseline state value)
-                    _, b = model.predict([x, R], batch_size=1)
-                    #print "v:", b[0][0, 0]
-                    returns = discount(rewards, b[0, 0, 0])
+                observations, actions, rewards, returns = fifo.get(timeout=args.queue_timeout)
 
                 # add to batch
                 batch_observations.append(np.array(observations))
@@ -169,20 +174,21 @@ def trainer(model, fifos, shared_array):
 
             # form training data from observations, actions and returns
             x = np.array(batch_observations)
-            y = np.array(batch_actions)
-            R = np.array(batch_returns)
-            R = R[..., np.newaxis]
+            p = np.array(batch_actions)
+            g = np.array(batch_returns)
+            g = g[..., np.newaxis]
             #print x.shape, y.shape, r.shape, b.shape
             #print "x:", x
             #print "y:", y
-            #print "R:", R
+            #print "g:", g
 
             # train the model
-            total_loss, policy_loss, baseline_loss = model.train_on_batch([x, R], [y, R])
+            total_loss, policy_loss, baseline_loss = model.train_on_batch([x, g], [p, g])
             #print "total_loss:", total_loss
             #print "policy_loss:", policy_loss
             #print "baseline_loss:", baseline_loss
-            shared_array.raw = pickle.dumps(model.get_weights(), pickle.HIGHEST_PROTOCOL)
+            # share model parameters
+            shared_buffer.raw = pickle.dumps(model.get_weights(), pickle.HIGHEST_PROTOCOL)
 
             step += 1
 
@@ -199,11 +205,11 @@ if __name__ == '__main__':
     parser.add_argument('--tau', type=float, default=0.1)
     # parallelization
     parser.add_argument('--num_runners', type=int, default=2)
-    parser.add_argument('--queue_length', type=int, default=5)
-    parser.add_argument('--queue_timeout', type=int, default=100)
+    parser.add_argument('--queue_length', type=int, default=2)
+    parser.add_argument('--queue_timeout', type=int, default=10)
     # how long
     parser.add_argument('--max_episodes', type=int, default=200)
-    parser.add_argument('--max_timesteps', type=int, default=1000)
+    parser.add_argument('--max_timesteps', type=int, default=200)
     # technical
     parser.add_argument('--display', action='store_true', default=False)
     parser.add_argument('--no_display', dest='display', action='store_false')
@@ -215,12 +221,15 @@ if __name__ == '__main__':
     env = gym.make(args.environment)
     assert isinstance(env.observation_space, Box)
     assert isinstance(env.action_space, Discrete)
+
     # create main model
-    model = create_model(env)
+    model = create_model(env, args)
     model.summary()
+
+    # create shared buffer for sharing weights
     blob = pickle.dumps(model.get_weights(), pickle.HIGHEST_PROTOCOL)
-    shared_array = Array('c', len(blob))
-    shared_array.raw = blob
+    shared_buffer = Array('c', len(blob))
+    shared_buffer.raw = blob
     env.close()
 
     # create fifos and threads for all runners
@@ -228,7 +237,7 @@ if __name__ == '__main__':
     for i in range(args.num_runners):
         fifo = Queue(args.queue_length)
         fifos.append(fifo)
-        process = Process(target=runner, args=(shared_array, fifo))
+        process = Process(target=runner, args=(shared_buffer, fifo, args))
         process.start()
 
-    trainer(model, fifos, shared_array)
+    trainer(model, fifos, shared_buffer, args)
